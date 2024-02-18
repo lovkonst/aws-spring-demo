71b5c56abf17d5ef0eec4c33c3dad79eb8b03ced
lovkonst <158325448+lovkonst@users.noreply.github.com>
2024-02-18 13:38:50 +0100

    added test for check job (#4)
    * added test for check job

---
+++ src/test/java/rs/lovkonst/awsspringdemo/SomeTest.java
@@ -0,0 +1,13 @@
+package rs.lovkonst.awsspringdemo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SomeTest {
+
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
