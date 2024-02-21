938f680cb0dc413f0eacd4a83311ab064d8df81e
lovtsovkonst007 <lovtsovkonst007@gmail.com>
2024-02-21 19:39:42 +0100

    Added 'help' endpoint

--- src/main/java/rs/lovkonst/awsspringdemo/controller/MainController.java
+++ src/main/java/rs/lovkonst/awsspringdemo/controller/MainController.java
@@ -3,17 +3,25 @@ package rs.lovkonst.awsspringdemo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import rs.lovkonst.awsspringdemo.entity.Endpoint;
+
+import java.util.List;
 
 @RestController
 public class MainController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello, Aws";
+        return "Hello, Aws<br> For more information go to '/help'";
+    }
+
+    @GetMapping("/help")
+    public List<Endpoint> help() {
+        return List.of(new Endpoint("/user/{name}", "Provide information for user"));
     }
 
     @GetMapping("/user/{name}")
-    public String userGreeting(@PathVariable String name) {
-        return String.format("Nice to see you, %s!", name);
+    public String userGreeting(@PathVariable("name") String username) {
+        return String.format("Nice to see you, %s!", username);
     }
 }
